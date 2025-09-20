5907d8ed6a17a0812648445a66527c32c4f17847
Acorx <134443324+Acorx@users.noreply.github.com>
2025-09-20 20:29:45 +0200

    Ajoute la configuration de Tailwind CSS

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          500: "#3B82F6",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
